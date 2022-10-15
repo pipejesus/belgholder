af3d8288aa8cd1cf1be2a121b193722630568fd8
Greg <grzegorz.karol.jasinski@gmail.com>
2022-10-15 03:45:41 +0200

    add test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pipejesus/belgholder
+
+go 1.19
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello")	
+	handler := http.HandlerFunc(handleRequest)
+	http.Handle("/photo", handler)
+	http.ListenAndServe(":30472", nil)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	fileBytes, err := ioutil.ReadFile("test.png")
+	if err != nil {
+		panic(err)
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(fileBytes)
+	return
 }
